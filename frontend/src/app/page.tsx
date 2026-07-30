327d24bc84d10c315afbedc0dfb7222204c6689f
CrackyP <cracky.craig@gmail.com>
2026-07-30 15:17:31 -0400

    Fix JSX syntax error in page.tsx drawer container

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -45,7 +45,7 @@ export default function HomePage() {
             <div
               className={`md:hidden fixed inset-0 z-[60] transform transition-transform duration-200 ease-out ${
                 mobileMenuOpen ? "translate-x-0" : "-translate-x-full"
-              }}`>
+              }`}
             >
               {/* Backdrop */}
               <div
